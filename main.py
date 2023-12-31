6257f1edc83a801410e1fc33526ff7ff2823f393
Do Xuan Long <longdo@dobtech.vn>
2024-01-01 00:14:12 +0800

    added memory to agent executor

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from langchain.prompts import (
 )
 from langchain.schema import SystemMessage
 from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
+from langchain.memory import ConversationBufferMemory
 from dotenv import load_dotenv
 
 from tools.sql import run_query_tool, list_tables, describe_table_tool
@@ -25,9 +26,15 @@ prompt = ChatPromptTemplate(
                 "Do not make any assumptions about what tables exist "
                 "or what columns exist. Instead, use the 'describe_tables' function"
             )),
+        MessagesPlaceholder(variable_name="chat_history"),
         HumanMessagePromptTemplate.from_template("{input}"),
         MessagesPlaceholder(variable_name="agent_scratchpad")
     ]
+)
+
+memory = ConversationBufferMemory(
+    memory_key="chat_history",
+    return_messages=True
 )
 
 tools = [
@@ -45,9 +52,14 @@ agent = OpenAIFunctionsAgent(
 agent_executor = AgentExecutor(
     agent=agent,
     verbose=True,
-    tools=tools
+    tools=tools,
+    memory=memory
 )
 
 # agent_executor("How many users have provided a shipping address?")
 agent_executor(
-    "Summarize the top 5 most popular products. Write the results to a report file.")
+    "How many orders are there? Write the result to an html report.")
+
+agent_executor(
+    "Repeat the exact same process for users."
+)
